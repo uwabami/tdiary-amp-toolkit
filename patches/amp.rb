5874c55b359557f95009d6a915579ecfc8fa9be8
Youhei SASAKI <uwabami@gfd-dennou.org>
2018-11-02 13:45:55 +0900

    Add ogp prefix into head
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- patches/amp.rb
+++ patches/amp.rb
@@ -17,6 +17,7 @@ module TDiary
 			.gsub(/^<html lang="ja-JP">/,'<html amp lang=ja>')
 			.gsub(/^<html lang="en-US">/,'<html amp lang=en>')
 			.gsub(/^<html>/,'<html amp>')
+			.gsub(/^<head>/,'<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">')
 			.gsub(/^\t(<meta name="generator")/,
 					"\t<script async src=\"https://cdn.ampproject.org/v0.js\"></script>\n\t\\1")
 			.gsub(/(width=device-width,initial-scale=1)/,'\\1,minimum-scale=1')
